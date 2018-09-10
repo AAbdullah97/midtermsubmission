13e8f93ef2407fe62d7c766b8561ac31cf352b80
AAbdullah97 <abumsa786gmail.com>
2018-09-10 07:49:11 -0400

    Mathematical Problem Solution

--- src/math/problems/MakePyramid.java
+++ src/math/problems/MakePyramid.java
@@ -1,7 +1,52 @@
 package math.problems;
 
 public class MakePyramid {
+
+
+    // Function to demonstrate printing pattern
+    public static void printTriagle(int n)
+    {
+        // number of spaces
+        int k = 2*n - 2;
+
+        // outer loop to handle number of rows
+        //  n in this case
+        for (int i=0; i<n; i++)
+        {
+
+            // inner loop to handle number spaces
+            // values changing acc. to requirement
+            for (int j=0; j<k; j++)
+            {
+                // printing spaces
+                System.out.print(" ");
+            }
+
+            // decrementing k after each loop
+            k = k - 1;
+
+            //  inner loop to handle number of columns
+            //  values changing acc. to outer loop
+            for (int j=0; j<=i; j++ )
+            {
+                // printing stars
+                System.out.print("* ");
+            }
+
+            // ending line after each row
+            System.out.println();
+        }
+
+    }
+
+
     public static void main(String[] args) {
+
+            int n = 5;
+            printTriagle(n);
+        }
+
+    }
 
         /*   Implement a large Pyramid of stars in the screen with java.
 
@@ -15,5 +60,5 @@ public class MakePyramid {
         */
 
 
-        }
-  }
+
+
