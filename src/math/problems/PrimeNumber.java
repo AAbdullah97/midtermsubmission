18e5b0ddbd8d5b94e64a566fc9919a5033431bb1
AAbdullah97 <abumsa786gmail.com>
2018-09-10 08:07:46 -0400

    Mathematical Problem Solution

--- src/math/problems/PrimeNumber.java
+++ src/math/problems/PrimeNumber.java
@@ -3,6 +3,35 @@ package math.problems;
 public class PrimeNumber {
 
 	public static void main(String[] args) {
+
+		int i =0;
+		int num =0;
+		//Empty String
+		String  primeNumbers = "";
+
+		for (i = 2; i <= 10000; i++)
+		{
+			int counter=0;
+			for(num =i; num>=1; num--)
+			{
+				if(i%num==0)
+				{
+					counter = counter + 1;
+				}
+			}
+			if (counter ==2)
+			{
+				//Appended the Prime number to the String
+				primeNumbers = primeNumbers + i + " ";
+			}
+		}
+		System.out.println("Prime numbers from 2 to 10000 are :");
+		System.out.println(primeNumbers);
+
+
+
+
+
 		/*
 		 * Find list of Prime numbers from number 2 to 1 million.
 		 * Try the best solution as possible.Which will take less CPU life cycle.
