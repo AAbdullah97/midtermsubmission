7c21d687fdee2cec2e0bfbe44911d47eec4a44ce
AAbdullah97 <abumsa786gmail.com>
2018-09-11 06:59:32 -0400

    String Problem Solved

--- src/string/problems/Permutation.java
+++ src/string/problems/Permutation.java
@@ -3,14 +3,36 @@ package string.problems;
 /**
  * Created by mrahman on 04/22/17.
  */
+
+/*
+ * Permutation of String "ABC" is "ABC" "BAC" "BCA" "ACB" "CAB" "CBA".
+ * Write Java program to compute all Permutation of a String
+ *
+ */
 public class Permutation {
 
-    public static void main(String[] args) {
+    static public void StringPermutation(String input)
+            {
+                StringPermutation("", input);
+            }
 
-        /*
-         * Permutation of String "ABC" is "ABC" "BAC" "BCA" "ACB" "CAB" "CBA".
-         * Write Java program to compute all Permutation of a String
-         *
-         */
-    }
-}
+            private static void StringPermutation(String permutation, String input)
+            {
+                if(input.length() == 0)
+                {
+                    System.out.println(permutation);
+                }
+                else
+                {
+                    for (int i = 0; i < input.length(); i++)
+                    {
+                        StringPermutation(permutation+input.charAt(i), input.substring(0, i)+input.substring(i+1, input.length()));
+                    }
+                }
+            }
+
+            public static void main(String[] args)
+            {
+                StringPermutation("GIT");
+            }
+        }
